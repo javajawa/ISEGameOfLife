d61b38e169c74db74a83f0b052b480966aeb950a
ChristmasRights <chris.fonseka@googlemail.com>
2011-05-27 13:42:01 +0100

    Fixed formatting in AgentDataModel

--- src/ise/gameoflife/AgentDataModel.java
+++ src/ise/gameoflife/AgentDataModel.java
@@ -5,57 +5,62 @@ import presage.abstractparticipant.APlayerDataModel;
 
 /**
  *
- * @author christopherfonseka
+ * @author Christopher Fonseka
  */
-public class AgentDataModel extends APlayerDataModel {
-    
-    //stores amount of food owned by agent
-    @Element
-            private int foodInPossesion;
-    
-    @Deprecated
-    public AgentDataModel() {
-        super();
-    }
-    
-    public AgentDataModel(int food) {
-        
-        this.foodInPossesion = food;
-    }
-    
-    @Override
-    public void onInitialise() {
-        
-    }
+public class AgentDataModel extends APlayerDataModel
+{
 
-    /**
-     * @return the foodInPossesion
-     */
-    public int getFoodInPossesion() {
-        return foodInPossesion;
-    }
+	//stores amount of food owned by agent
+	@Element
+	private int foodInPossesion;
 
-    /**
-     * @param foodInPossesion the foodInPossesion to set
-     */
-    public void setFoodInPossesion(int foodInPossesion) {
-        this.foodInPossesion = foodInPossesion;
-    }
-    
-    
-    /**
-     * @param consumed reduces the foodInPossesion by a given amount
-     */
-    public void foodConsumed(int consumed) {
-        this.foodInPossesion -= consumed;
-    }
-    
-    /**
-     * @param acquired increases food inventory by given amount
-     */
-    public void foodAquired(int acquired) {
-        this.foodInPossesion += acquired;
-    }
-    
-    
+	@Deprecated
+	public AgentDataModel()
+	{
+		super();
+	}
+
+	public AgentDataModel(int food)
+	{
+
+		this.foodInPossesion = food;
+	}
+
+	@Override
+	public void onInitialise()
+	{
+	}
+
+	/**
+	 * @return the foodInPossesion
+	 */
+	public int getFoodInPossesion()
+	{
+		return foodInPossesion;
+	}
+
+	/**
+	 * @param foodInPossesion the foodInPossesion to set
+	 */
+	public void setFoodInPossesion(int foodInPossesion)
+	{
+		this.foodInPossesion = foodInPossesion;
+	}
+
+	/**
+	 * @param consumed reduces the foodInPossesion by a given amount
+	 */
+	public void foodConsumed(int consumed)
+	{
+		this.foodInPossesion -= consumed;
+	}
+
+	/**
+	 * @param acquired increases food inventory by given amount
+	 */
+	public void foodAquired(int acquired)
+	{
+		this.foodInPossesion += acquired;
+	}
+
 }
