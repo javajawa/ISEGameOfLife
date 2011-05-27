741092b1866ce1dd6d53735a134f75e96082fdd8
ChristmasRights <chris.fonseka@googlemail.com>
2011-05-27 13:36:21 +0100

    Class completed

--- src/ise/gameoflife/FoodDataModel.java
+++ src/ise/gameoflife/FoodDataModel.java
@@ -17,18 +17,42 @@ public class FoodDataModel
 	 * Name of food type
 	 */
 	@Element
-	protected String name;
+	private String name;
 	/*
 	 * Nutritional value of the food
 	 */
 	@Element
-	protected int nutrition;
+	private int nutrition;
 	/*
 	 * Number of agents required to obtain food
 	 * Extends later into probabilities
 	 */
 	@Element
-	protected int huntersRequired;
+	private int huntersRequired;
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @return the nutrition
+	 */
+	public int getNutrition()
+	{
+		return nutrition;
+	}
+
+	/**
+	 * @return the huntersRequired
+	 */
+	public int getHuntersRequired()
+	{
+		return huntersRequired;
+	}
 	
 					
 }
