e75f94d967d0bc3df2004a836649016e5af0c5cd
ChristmasRights <chris.fonseka@googlemail.com>
2011-05-27 13:09:47 +0100

    Define AgentData Model

---
+++ src/ise/gameoflife/AgentDataModel.java
@@ -0,0 +1,61 @@
+package ise.gameoflife;
+
+import org.simpleframework.xml.Element;
+import presage.abstractparticipant.APlayerDataModel;
+
+/**
+ *
+ * @author christopherfonseka
+ */
+public class AgentDataModel extends APlayerDataModel {
+    
+    //stores amount of food owned by agent
+    @Element
+            private int foodInPossesion;
+    
+    @Deprecated
+    public AgentDataModel() {
+        super();
+    }
+    
+    public AgentDataModel(int food) {
+        
+        this.foodInPossesion = food;
+    }
+    
+    @Override
+    public void onInitialise() {
+        
+    }
+
+    /**
+     * @return the foodInPossesion
+     */
+    public int getFoodInPossesion() {
+        return foodInPossesion;
+    }
+
+    /**
+     * @param foodInPossesion the foodInPossesion to set
+     */
+    public void setFoodInPossesion(int foodInPossesion) {
+        this.foodInPossesion = foodInPossesion;
+    }
+    
+    
+    /**
+     * @param consumed reduces the foodInPossesion by a given amount
+     */
+    public void foodConsumed(int consumed) {
+        this.foodInPossesion -= consumed;
+    }
+    
+    /**
+     * @param acquired increases food inventory by given amount
+     */
+    public void foodAquired(int acquired) {
+        this.foodInPossesion += acquired;
+    }
+    
+    
+}
