738544dc03ba08d3f3fc1b20c6a0b4bcaac0411b
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-05-28 13:09:37 +0100

    Added RunSimulation class, and references to Run time libraries

---
+++ src/ise/gameoflife/RunSimulation.java
@@ -0,0 +1,20 @@
+package ise.gameoflife;
+
+import presage.gui.ControlCenter;
+
+/**
+ *
+ * @author Benedict
+ */
+public class RunSimulation
+{
+
+	private RunSimulation()
+	{
+	}
+
+	public static void main(String args[])
+	{
+		ControlCenter.main(args);
+	}
+}
