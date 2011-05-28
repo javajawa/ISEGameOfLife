699929f8c6577c4d6c270e2170a38722dca6f5fc
Olly <OGHill@googlemail.com>
2011-05-28 15:02:06 +0100

    Documented GenericInput.java
    Signed-off-by: Olly <OGHill@googlemail.com>

--- src/ise/gameoflife/inputs/GenericInput.java
+++ src/ise/gameoflife/inputs/GenericInput.java
@@ -6,30 +6,49 @@ import presage.Input;
 /**
  * Base class for dealing with inputs, which implements all the required 
  * functions
- * FIXME: Document this
  * @author Benedict Harcourt
  */
 abstract class GenericInput implements Input
 {
+	/**
+	 * Timestamp: the time at which the input event occurred, in terms of 
+	 * simulation time
+	 * Performative: A String describing the current state/action of the input
+	 * eg: when an ActionError occurs, the performative is set to "ActionError"
+	 */
 	protected long timestamp;
 	protected String performative;
-
+	
+/**
+	 * Creates a new instance of GenericInput
+	 * @param timestamp
+	 * @param performative 
+	 */
 	public GenericInput(long timestamp, String performative)
 	{
 		this.timestamp = timestamp;
 		this.performative = performative;
 	}
-
+/**
+	 * Returns the Timestamp of the input event
+	 * @return 
+	 */
 	public long getTimestamp()
 	{
 		return timestamp;
 	}
-
+/**
+	 * Set Timestamp
+	 * @param timestamp 
+	 */
 	public void setTimestamp(long timestamp)
 	{
 		this.timestamp = timestamp;
 	}
-
+/**
+	 * Returns the performative
+	 * @return 
+	 */
 	public String getPerformative()
 	{
 		return performative;
