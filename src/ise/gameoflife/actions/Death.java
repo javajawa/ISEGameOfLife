9fc028035948e11e59b2022d9c3b528720e5bd76
Olly <OGHill@googlemail.com>
2011-05-28 16:59:55 +0100

    Added Death.java class
    Signed-off-by: Olly <OGHill@googlemail.com>

---
+++ src/ise/gameoflife/actions/Death.java
@@ -0,0 +1,15 @@
+package ise.gameoflife.actions;
+
+import presage.Action;
+import presage.events.CoreEvents.DeactivateParticipant;
+
+/**
+ * @author Olly
+ * Implements an instance of the Death message class
+ */
+public class Death implements Action
+{
+	public Death(){
+		super();
+	}
+}
