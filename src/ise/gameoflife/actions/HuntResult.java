762c2ad01a482fed04c9280f777da53be4d11f60
Olly <OGHill@googlemail.com>
2011-05-28 14:10:29 +0100

    Added HuntResult class
    Signed-off-by: Olly <OGHill@googlemail.com>
    
    HuntResult class deals with the result of a Hunt action - primarily,
    the food gained from the hunt.

---
+++ src/ise/gameoflife/actions/HuntResult.java
@@ -0,0 +1,42 @@
+package ise.gameoflife.actions;
+
+import java.util.UUID;
+import presage.Input;
+
+/**
+ *
+ * @author Olly
+ */
+public class HuntResult implements Input
+{
+	private UUID HuntResult;
+	
+	private double nutrition;
+	
+	/**
+	 * Creates a new instance of the HuntResult class, which gives the result of a 
+	 * Hunt action.
+	 * @param id
+	 * @param nutrition 
+	 */
+	public HuntResult(UUID id, double nutrition){
+		super();
+		this.HuntResult = id;
+		this.nutrition = nutrition;
+	}
+	/**
+	 * Returns nutrition value of food hunted
+	 * @return 
+	 */
+	public double getNutritionValue(){
+		return nutrition;
+	}
+	/**
+	 * Returns UUID of the hunt result
+	 * used as token to enable message passing
+	 * @return 
+	 */
+	public UUID getHuntResultUUID(){
+		return HuntResult;
+	}
+}
