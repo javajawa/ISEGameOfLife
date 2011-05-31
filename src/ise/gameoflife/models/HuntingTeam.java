beaca55d9f9f43fc404605f3af8ec2d42278e99e
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-05-31 13:13:23 +0100

    Initial implementation of HuntingTeam

--- src/ise/gameoflife/models/HuntingTeam.java
+++ src/ise/gameoflife/models/HuntingTeam.java
@@ -1,14 +1,49 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package ise.gameoflife.models;
 
+import ise.gameoflife.enviroment.EnvConnector;
+import java.util.Collections;
+import java.util.List;
+
 /**
- *
- * @author benedict
+ * TODO: Documentation
+ * @author Benedict Harcourt
  */
 public class HuntingTeam
 {
+	private List<String> members;
+	private Food orderedTarget;
+
+	/**
+	 * TODO: Documentation
+	 * @param ec
+	 * @param members
+	 * @param orderedTarget
+	 */
+	public HuntingTeam(EnvConnector ec, List<String> members, Food orderedTarget)
+	{
+		// TODO: Using the EnvConnector, check that all members of memebers are 
+		// hunters before committing it to the object
+		this.members = Collections.unmodifiableList(members);
+		this.orderedTarget = orderedTarget;
+	}
+
+	/**
+	 * TODO: Documentation
+	 * @return
+	 */
+	public Food getOrderedTarget()
+	{
+		return orderedTarget;
+	}
+
+	/**
+	 * TODO: Documentation
+	 * @return
+	 */
+	@SuppressWarnings("ReturnOfCollectionOrArrayField")
+	public List<String> getMembers()
+	{
+		return members;
+	}
 	
 }
