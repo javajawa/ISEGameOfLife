97217836be73d6ab79547d6be91249850c418b5e
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-05-31 18:17:20 +0100

    Sketched out helper class for building a set of simulations using reflection

---
+++ src/ise/gameoflife/BuildSimulations.java
@@ -0,0 +1,59 @@
+package ise.gameoflife;
+
+import ise.gameoflife.simulatons.DoubleAgent;
+import ise.gameoflife.simulatons.SingleAgent;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * TODO: Documentation
+ * TODO: Make this thing work (it should do... :'( )
+ * @author Benedict
+ */
+final class BuildSimulations
+{
+
+	private static final Class<?>[] simulationClasses = {
+		SingleAgent.class,
+		DoubleAgent.class
+	};
+	
+	private BuildSimulations()
+	{
+		// Nothing to see here. Move along, citizen!
+	}
+	
+	public static void main(String args[])
+	{
+
+		for (Class<?> c : simulationClasses)
+		{
+			try
+			{
+				c.getMethod("main", args.getClass()).invoke(null, (Object[])args);
+			}
+			catch (IllegalAccessException ex)
+			{
+				Logger.getLogger(BuildSimulations.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			catch (IllegalArgumentException ex)
+			{
+				Logger.getLogger(BuildSimulations.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			catch (InvocationTargetException ex)
+			{
+				Logger.getLogger(BuildSimulations.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			catch (NoSuchMethodException ex)
+			{
+				Logger.getLogger(BuildSimulations.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			catch (SecurityException ex)
+			{
+				Logger.getLogger(BuildSimulations.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
+}
