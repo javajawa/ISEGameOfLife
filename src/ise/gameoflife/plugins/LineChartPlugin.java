3b90ed24b26b24e7fa94cdc33a360963e98a2ef8
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-05-31 09:14:55 +0100

    Altered Formatting and cleaned up a couple of warnings in LineChartPlugin

--- src/ise/gameoflife/plugins/LineChartPlugin.java
+++ src/ise/gameoflife/plugins/LineChartPlugin.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import java.util.SortedSet;
@@ -33,138 +32,162 @@ import ise.gameoflife.enviroment.EnvironmentDataModel;
  *
  * @author harry
  */
-public class LineChartPlugin extends JPanel implements Plugin {
-    private static final long serialVersionUID = 1L;
+public class LineChartPlugin extends JPanel implements Plugin
+{
 
-	Simulation sim;
+	private static final long serialVersionUID = 1L;
+
+	private final static String title = "Population";
+	private final static String xaxis = "Simulation cycle";
+	private final static String yaxis = "Num People";
+	private final static String label = "Num People";
+
+	private Simulation sim;
+	private EnvironmentDataModel dm;
 
 	@Element
-	String outputpath;
-
+	private String outputpath;
 	@Element
-	int outputwidth;
-
+	private int outputwidth;
 	@Element
-	int outputheight;
+	private int outputheight;
 
-	String title = "Population";
+	private XYSeriesCollection data;
+	private XYSeriesCollection datatempcopy;
+	private JPanel control = new JPanel();
+	private ChartPanel chartPanel;
 
-	String xaxis = "Simulation cycle";
-
-	String yaxis = "Num People";
-
-	String label = "Num People";
-
-	EnvironmentDataModel dm;
-
-	XYSeriesCollection data;
-
-	// ChartPanel chartPanel;
-
-	XYSeriesCollection datatempcopy;
-
-	JPanel control = new JPanel();
-
-	ChartPanel chartPanel;
-
-	public LineChartPlugin() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * SimpleXML no-arg Constructor. Do not use this constructor, it is only for
+	 * the purpose of SimpleXML being able to create the object through inflection
+	 * @deprecated Serialisation constructor
+	 */
+	@Deprecated
+	public LineChartPlugin()
+	{
+		// Nothing to see here. Move along, citizen.
 	}
 
-	@PluginConstructor( { "outputpath", "outputwidth", "outputheight"})
+	/**
+	 * Creates a new instance of the LineChartPlugin
+	 * @param outputpath Path to write the final image to
+	 * @param outputwidth Width of the outputted image
+	 * @param outputheight Height of the outputted image
+	 */
+	@PluginConstructor(
+	{
+		"outputpath", "outputwidth", "outputheight"
+	})
 	public LineChartPlugin(String outputpath, int outputwidth,
-			int outputheight) {
+					int outputheight)
+	{
 		super();
 		this.outputpath = outputpath;
 		this.outputwidth = outputwidth;
 		this.outputheight = outputheight;
 	}
 
-	private double getNumHunters() {
-                SortedSet<String> participantIdSet = sim.getactiveParticipantIdSet("hunter");
-                return participantIdSet.size();
+	private double getNumHunters()
+	{
+		SortedSet<String> participantIdSet = sim.getactiveParticipantIdSet("hunter");
+		return participantIdSet.size();
 	}
 
-        @Override
-	public void execute() {
+	@Override
+	public void execute()
+	{
 
 		// simply get the environment datamodel
-		dm = (EnvironmentDataModel) sim.getEnvDataModel();
+		dm = (EnvironmentDataModel)sim.getEnvDataModel();
 
 		double population = getNumHunters();
 
-		synchronized (this) {
+		synchronized (this)
+		{
 
 			XYSeries series;
-			try {
+			try
+			{
 				series = data.getSeries("population");
 				series.add(dm.time, population);
-			} catch (org.jfree.data.UnknownKeyException e) {
+			}
+			catch (org.jfree.data.UnknownKeyException e)
+			{
 				series = new XYSeries("population");
 				data.addSeries(series);
 				series.add(dm.time, population);
 			}
 
 			//if (dmodel.time % 100 == 0) {
-				// this will cause the gui to update?
-				updateChart();
+			// this will cause the gui to update?
+			updateChart();
 			//}
 		}
 	}
 
-	public void updateChart() {
+	public void updateChart()
+	{
 
 		this.remove(chartPanel);
 
-		synchronized (this) {
+		synchronized (this)
+		{
 			datatempcopy = clonedata(data);
 		}
 
 		chartPanel = newChart(datatempcopy);
 
-		add(chartPanel, (new BorderLayout()).CENTER);
+		add(chartPanel, BorderLayout.CENTER);
 
 		chartPanel.updateUI();
 	}
 
-	public XYSeriesCollection clonedata(XYSeriesCollection data) {
+	public XYSeriesCollection clonedata(XYSeriesCollection data)
+	{
 
-		try {
-			return (XYSeriesCollection) ObjectCloner.deepCopy(data);
-		} catch (Exception e) {
+		try
+		{
+			return (XYSeriesCollection)ObjectCloner.deepCopy(data);
+		}
+		catch (Exception e)
+		{
 			System.err.println("Exception in execute() - "
-					+ data.getClass().getCanonicalName()
-					+ " is not serializable" + e);
+							+ data.getClass().getCanonicalName()
+							+ " is not serializable" + e);
 			return null;
 		}
 	}
 
-	public ChartPanel newChart(XYSeriesCollection datatempcopy) {
+	public ChartPanel newChart(XYSeriesCollection datatempcopy)
+	{
 
 		JFreeChart chart = ChartFactory.createXYLineChart(title, xaxis, yaxis,
-				datatempcopy, PlotOrientation.VERTICAL, true, true, false);
+						datatempcopy, PlotOrientation.VERTICAL, true, true, false);
 
 		chartPanel = new ChartPanel(chart);
 		chartPanel.setPreferredSize(new java.awt.Dimension(this.getWidth(),
-				this.getHeight()));
+						this.getHeight()));
 
 		return chartPanel;
 
 	}
 
-        @Override
-	public String getLabel() {
+	@Override
+	public String getLabel()
+	{
 		// TODO Auto-generated method stub
 		return label;
 	}
 
-        @Override
-	public String getShortLabel() {
+	@Override
+	public String getShortLabel()
+	{
 		return label;
 	}
 
-        @Override
-	public void initialise(Simulation sim) {
+	@Override
+	public void initialise(Simulation sim)
+	{
 		System.out.println(" -Initialising....");
 
 		this.sim = sim;
@@ -182,55 +205,69 @@ public class LineChartPlugin extends JPanel implements Plugin {
 
 		JButton updateButton = new JButton("Update Graph");
 
-		updateButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
+		updateButton.addActionListener(new ActionListener()
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent ae)
+			{
 				updateChart();
 			}
+
 		});
 
 		//control.add(label);
 		control.add(updateButton);
 
 		this.setLayout(new BorderLayout());
-		add(control, (new BorderLayout()).NORTH);
-		add(chartPanel, (new BorderLayout()).CENTER);
+		add(control, BorderLayout.NORTH);
+		add(chartPanel, BorderLayout.CENTER);
 	}
 
-        @Override
-	public void onDelete() {
+	@Override
+	public void onDelete()
+	{
 		// TODO Auto-generated method stub
-
 	}
 
-        @Override
-	public void onSimulationComplete() {
+	@Override
+	public void onSimulationComplete()
+	{
 
 		this.removeAll();
 
-		synchronized (this) {
+		synchronized (this)
+		{
 			chartPanel = newChart(data);
 		}
 
 		this.setLayout(new BorderLayout());
-		add(chartPanel, (new BorderLayout()).CENTER);
+		add(chartPanel, BorderLayout.CENTER);
 
 		chartPanel.updateUI();
 
 		File file;
 
-		try {
+		try
+		{
 			file = new File(outputpath);
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			System.err.println("Graph Plugin " + label
-					+ " : Invalid output path" + e);
+							+ " : Invalid output path" + e);
 			return;
 		}
 
-		try {
-			ChartUtilities.saveChartAsPNG(file, chartPanel.getChart(), this.outputwidth,
-					this.outputheight);
-		} catch (Exception e) {
-			System.out.println("Problem occurred creating chart." + label);
+		try
+		{
+			ChartUtilities.saveChartAsPNG(file, chartPanel.getChart(),
+							this.outputwidth,
+							this.outputheight);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Problem occurred creating chart " + label + ": " + e.getMessage());
 		}
 
 	}
