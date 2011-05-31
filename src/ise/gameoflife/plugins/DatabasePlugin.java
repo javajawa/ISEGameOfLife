3e7a91e030491de91c5934f84304c28f19c3deac
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-05-31 16:48:55 +0100

    (Re)Instated SerialVersionUID in DatabasePlugin

--- src/ise/gameoflife/plugins/DatabasePlugin.java
+++ src/ise/gameoflife/plugins/DatabasePlugin.java
@@ -23,7 +23,7 @@ import java.sql.Statement;
  */
 public class DatabasePlugin extends JPanel implements Plugin {
 
-    //private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private final static String title = "DatabasePlugin";
     private final static String label = "DatabasePlugin";
