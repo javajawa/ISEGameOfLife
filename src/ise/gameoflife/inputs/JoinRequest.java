db5c909eb1b6116ebaed9037d7f98b0db37451b7
@XmasRights <chris.fonseka@googlemail.com>
2011-06-01 12:09:35 +0100

    Join Request documented

--- src/ise/gameoflife/inputs/JoinRequest.java
+++ src/ise/gameoflife/inputs/JoinRequest.java
@@ -1,19 +1,33 @@
 package ise.gameoflife.inputs;
 
 /**
- * TODO: Documentation for this Class
+ * After considering the balance between work and play, this class specified
+ * how an agent goes about choosing a group to be with for their term in
+ * the game
  * @author Benedict
  */
 public class JoinRequest extends GenericInput
 {
+	
+	//Candidate in question
 	private String agent;
 
+	
+	/**
+	 * Formalises the candidates request. A series of strange and
+	 * embarassing forms of "hazing" is sure to follow
+	 * @param time
+	 * @param agent 
+	 */
 	public JoinRequest(long time, String agent)
 	{
 		super(time, "joinrequest");
 		this.agent = agent;
 	}
 
+	/**
+	 * @return agent wanting to join the group
+	 */
 	public String getAgent()
 	{
 		return agent;
