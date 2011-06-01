3be26d31de75fd8cbab26ad53af822a07e3f5d12
Olly <OGHill@googlemail.com>
2011-06-01 15:10:02 +0100

    Documented DistrbuteFood.java
    Signed-off-by: Olly <OGHill@googlemail.com>

--- src/ise/gameoflife/actions/DistributeFood.java
+++ src/ise/gameoflife/actions/DistributeFood.java
@@ -3,25 +3,42 @@ package ise.gameoflife.actions;
 import presage.Action;
 
 /**
- * TODO: Document this class
+ * Distributes food amongst agents
  * @author Benedict
  */
 public class DistributeFood implements Action
 {
+	/**
+	 * the Agent receiving food
+	 */
 	private String agent;
+	/**
+	 * Amount of food agent receives
+	 */
 	private double amount;
 
+	/**
+	 * Distributes food to agent
+	 * @param agent agent receiving food
+	 * @param amount amount of food received
+	 */
 	public DistributeFood(String agent, double amount)
 	{
 		this.agent = agent;
 		this.amount = amount;
 	}
-
+	/**
+	 * Gets the Agent receiving food
+	 * @return agent receiving food
+	 */
 	public String getAgent()
 	{
 		return agent;
 	}
-
+	/**
+	 * Gets amount of food agent will receive
+	 * @return amount of food agent will receive
+	 */
 	public double getAmount()
 	{
 		return amount;
