d3a05253c7dd9ce888c6fe70712e8bae56992a71
@XmasRights <chris.fonseka@googlemail.com>
2011-06-02 14:30:23 +0100

    Group Data Initialiser documented

--- src/ise/gameoflife/models/GroupDataInitialiser.java
+++ src/ise/gameoflife/models/GroupDataInitialiser.java
@@ -1,18 +1,27 @@
 package ise.gameoflife.models;
 
 /**
- * TODO: DOcument this
+ * Group data intialiser is an object that contains all the information used
+ * to create a new group.
  * @author Benedict
  */
 public class GroupDataInitialiser
 {
 	private long randomSeed;
-
+	
+	/**
+	 * Initialises the random seed
+	 * @param randomSeed 
+	 */
 	public GroupDataInitialiser(long randomSeed)
 	{
 		this.randomSeed = randomSeed;
 	}
 
+	/**
+	 * Gets the random seed used to create groups
+	 * @return 
+	 */
 	public long getRandomSeed()
 	{
 		return randomSeed;
