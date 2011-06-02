46e42f61dc382abb63c3e4d435535826166879a6
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-02 14:30:00 +0100

    Documented PublicAgentDataModel.java
    Signed-off-by: Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>

--- src/ise/gameoflife/participants/PublicAgentDataModel.java
+++ src/ise/gameoflife/participants/PublicAgentDataModel.java
@@ -15,9 +15,15 @@ import java.io.Serializable;
 public final class PublicAgentDataModel implements PlayerDataModel, Serializable
 {
 	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Data Model which backs this public data model
+	 */
 	private AgentDataModel source;
 
+	/**
+	 * Constructor for PublicAgentDataModel
+	 * @param source the Data model which backs the public data model
+	 */
 	PublicAgentDataModel(AgentDataModel source)
 	{
 		this.source = source;
@@ -41,31 +47,47 @@ public final class PublicAgentDataModel implements PlayerDataModel, Serializable
 	{
 		return source.getFoodInPossesion();
 	}
-
+	/**
+	 * Gets time: the number of cycles (which are the subdivision of rounds) which have passed
+	 * @return the time (number of cycles which have passed)
+	 */
 	@Override
 	public long getTime()
 	{
 		return source.getTime();
 	}
-
+	/**
+	 * Sets the time
+	 * setTime should not used since the simulation deals with setting the time - calling this will have no effect.
+	 * @param time the time to set
+	 */
 	@Override
 	public void setTime(long time)
 	{
 		source.setTime(time);
 	}
-
+	/**
+	 * Returns the list of roles which the agent has
+	 * @return a list of roles
+	 */
 	@Override
 	public ArrayList<String> getRoles()
 	{
 		return source.getRoles();
 	}
-
+	/**
+	 * Sets all of the agents roles
+	 * @param roles: the list of roles to assign to the agent
+	 */
 	@Override
 	public void setRoles(ArrayList<String> roles)
 	{
 		source.setRoles(roles);
 	}
-
+	/**
+	 * Implemented in Presage and (currently) serves no purpose
+	 * @return: a null string, at the moment
+	 */
 	@Override
 	public String getPlayerClass()
 	{
