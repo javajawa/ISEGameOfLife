0bc683b99db76968e11793b91f85bbc97c90ac38
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-05 10:21:31 +0100

    Added a ValueScaler utility class

---
+++ src/ise/gameoflife/models/ValueScaler.java
@@ -0,0 +1,43 @@
+package ise.gameoflife.models;
+
+/**
+ *
+ * @author Benedict
+ */
+public class ValueScaler
+{
+	public static double scale(double old, double amount)
+	{
+		return scale(old, amount, 0.1);
+	}
+
+	/**
+	 * Allows the easy changing of a value between 0 and 1 by an number of jumps
+	 * whilst guaranteeing that the returned value will never fall outside of the
+	 * range 0 - 1. Also, The operation is (near enough) reversible, meaning that
+	 * the movement amount is actually representative.
+	 * 
+	 * The function is implemented in a similar way to Xeno's paradox - each jump
+	 * towards the end of the range is smaller than the previous space, as defined
+	 * by the space parameter. This parameter must lie within the range 0-1
+	 * @param old The old value that the scaled member had
+	 * @param amount The number of jumps to make
+	 * @param step The size of each jump
+	 * @return The new value, scaled between 0 - 1
+	 */
+	public static double scale(double old, double amount, double step)
+	{
+		if (step <= 0 || step >= 1) throw new IllegalArgumentException("Step must fall within the range 0 to 1, exlcusive");
+		if (amount < 0)
+		{
+			return 1-scale(1-old, -amount, step);
+		}
+		double space = 1-old;
+		space *= Math.pow(1-step, amount);
+		return 1-space;
+	}
+
+	private ValueScaler()
+	{
+	}
+}
