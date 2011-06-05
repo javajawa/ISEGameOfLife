fcac1a6d36068da4693e5cf21ee723820463d897
Xitong Gao <gxtfmx@gmail.com>
2011-06-05 21:03:31 +0100

    Added a basic Evolution implementation

---
+++ src/ise/gameoflife/genetics/Evolution.java
@@ -0,0 +1,93 @@
+package ise.gameoflife.genetics;
+
+import java.util.ArrayList;
+
+/**
+ * This class provides the framework for iterative evolution
+ * @author admko
+ */
+public abstract class Evolution
+	<Specie extends Evolvable, SpecieGenome extends Genome>
+{
+
+	abstract protected void evaluate();
+
+	abstract protected void select();
+	
+	public void evolve()
+	{
+		// parameter checks
+		if (this.iterations <= 0)
+		{
+			throw new RuntimeException("Invalid iteration count: " +
+					this.iterations + ". It must be > 0");
+		}
+		if (this.population <= 0)
+		{
+			throw new RuntimeException("Invalid population count: " +
+					this.population + ". It must be > 0");
+		}
+
+		// iterative genetic algorithm
+		for (int currentIteration = 1;
+			 currentIteration <= this.iterations;
+			 currentIteration++)
+		{
+			evaluate();
+			select();
+		}
+	}
+
+	// boilerplate code for setters & getters
+	// specieList
+	protected ArrayList<Specie> specieList = null;
+	public ArrayList<Specie> specieList()
+	{
+		if (null != specieList) return specieList;
+
+		specieList = new ArrayList<Specie>();
+		return specieList;
+	}
+	// genePool
+	protected GenePool<SpecieGenome> genePool = null;
+	public GenePool<SpecieGenome> genePool()
+	{
+		if (null != genePool) return genePool;
+		
+		genePool = new GenePool<SpecieGenome>();
+		return genePool;
+	}
+	// iterations
+	protected int iterations = -1;
+	public int iterations()
+	{
+		return this.iterations;
+	}
+	public void setIterations(int newIterations)
+	{
+		if (newIterations <= 0)
+		{
+			throw new RuntimeException("Invalid iteration count: " +
+					newIterations + ". It must be > 0");
+		}
+
+		this.iterations = newIterations;
+	}
+	// population
+	protected int population = -1;
+	public int population()
+	{
+		return this.population;
+	}
+	public void setPopulation(int newPopulation)
+	{
+		if (newPopulation <= 0)
+		{
+			throw new RuntimeException("Invalid population count: " +
+					newPopulation + ". It must be > 0");
+		}
+
+		this.population = newPopulation;
+	}
+
+}
