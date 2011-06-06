d4fe421ec392eaf45d4abaaca9b700c1f8ca1dd1
Theo <theodosis8@hotmail.com>
2011-06-06 01:17:59 +0100

    add all kind of agents in simulation

--- src/ise/gameoflife/simulatons/Politics.java
+++ src/ise/gameoflife/simulatons/Politics.java
@@ -30,13 +30,15 @@ public class Politics extends GenericSimulation
 		{
 			addAgent(new TestPoliticalAgent(20, 2, AgentType.R));
 			addAgent(new TestPoliticalAgent(20, 2, AgentType.TFT));
+                        addAgent(new TestPoliticalAgent(20, 2, AgentType.AC));
+                        addAgent(new TestPoliticalAgent(20, 2, AgentType.AD));
 		}
 	}
 
 	@Override
 	protected void foods()
 	{
-		addFood("Rabbit", 1, 1);
+		addFood("Rabbit", 2, 1);
 		addFood("Stag", 5, 2);
 	}
 
