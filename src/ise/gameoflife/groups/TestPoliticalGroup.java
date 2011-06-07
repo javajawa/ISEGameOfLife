f39ca7670df7748a32397567ee6d28e88731dee6
Aadil <aak08@imperial.ac.uk>
2011-06-07 18:57:19 +0100

    Political groups can now respond to a join request

--- src/ise/gameoflife/groups/TestPoliticalGroup.java
+++ src/ise/gameoflife/groups/TestPoliticalGroup.java
@@ -35,46 +35,56 @@ public class TestPoliticalGroup extends AbstractGroupAgent {
 	}
 
 	@Override
-	protected boolean respondToJoinRequest(String playerID) {              
-//            double heuristic;
-//
-//            //used for the socio-economic faction of heuristic
-//            double vectorDistance;
-//            double maxDistance = Math.sqrt(2);
-//            double economic, social, esFaction;
-//
-//            //used for the trust faction of heuristic
-//            double trustFaction, trustSum = 0;
-//            int numKnownTrustValues = 0;
-//
-//            //Obtain how much trust there is between the memberss of the group and the agent
-//            for (String trustor : this.getDataModel().getMemberList()) {
-//                    Double trustValue = this.getConn().getAgentById(trustor).getTrust(playerID);
-//                    if (trustValue != null) {
-//                            trustSum += trustValue;
-//                            numKnownTrustValues++;
-//                    }
-//            }
-//            if (numKnownTrustValues == 0) {
-//                return true;//this agent, playerID, wants to make a group of only himself
-//            }
-//            trustFaction = trustSum / numKnownTrustValues;
-//
-//            economic = this.getConn().getAgentById(playerID).getEconomicBelief() - this.getDataModel().getCurrentEconomicPoisition();//change in X
-//            social = this.getConn().getAgentById(playerID).getSocialBelief() - this.getDataModel().getEstimatedSocialLocation();//change in Y
-//            vectorDistance = Math.sqrt(Math.pow(economic, 2) + Math.pow(social, 2));
-//            esFaction = 1 - (vectorDistance / maxDistance);
-//
-//            heuristic = 0.5*trustFaction + 0.5*esFaction;
-//
-//            if (heuristic > 0.5) {
-//                return true;
-//            }
-//            else {
-//                return false;
-//            }
-            return true;
+	protected boolean respondToJoinRequest(String playerID) {            
+            //find out if the agent itself has just created this group
+            //if so then there is no need to compute a heuristic
+            if (getDataModel().getMemberList().isEmpty())
+                return true;
+            
+            //find out if the agent was invited into a newly created group
+            //if so then there is no need to to compute a heuristic
+            if (getDataModel().getMemberList().size() == 1)
+                return true;
+            
+            //otherwise we have the usual agent requesting entry to this group
+            double heuristic = 0;                
+            
+            //used for the socio-economic faction of heuristic
+            double vectorDistance;
+            double maxDistance = Math.sqrt(2);
+            double economic, social, esFaction = 0;
+
+            //used for the trust faction of heuristic
+            double trustFaction = 0, trustSum = 0;
+            int numKnownTrustValues = 0;
+
+            //Obtain how much trust there is between the members of the group and the agent
+            for (String trustor : this.getDataModel().getMemberList()) {
+                    Double trustValue = this.getConn().getAgentById(trustor).getTrust(playerID);
+                    if (trustValue != null) {
+                            trustSum += trustValue;
+                            numKnownTrustValues++;
+                    }
+            }
+            if (numKnownTrustValues != 0) {
+                trustFaction = trustSum / numKnownTrustValues;
+            }
+
+            economic = this.getConn().getAgentById(playerID).getEconomicBelief() - this.getDataModel().getCurrentEconomicPoisition();//change in X
+            social = this.getConn().getAgentById(playerID).getSocialBelief() - this.getDataModel().getEstimatedSocialLocation();//change in Y
+            vectorDistance = Math.sqrt(Math.pow(economic, 2) + Math.pow(social, 2));
+            esFaction = 1 - (vectorDistance / maxDistance);
+
+            heuristic = 0.5*trustFaction + 0.5*esFaction;
+
+            if (heuristic > 0.5) {
+                return true;
+            }
+            else {
+                return false;
+            }
 	}
+        
 	/**
 	 * Determines the optimum hunting choice in terms of the food gained/hunters needed ratio
 	 * divides the group up into teams of this size, then passes them the order to hunt that food.
