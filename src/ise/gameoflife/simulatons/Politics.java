ffede39dc18c6c70f2c4b208f755a0ee17226fd8
Theo <theodosis8@hotmail.com>
2011-06-09 15:55:58 +0100

    test

--- src/ise/gameoflife/simulatons/Politics.java
+++ src/ise/gameoflife/simulatons/Politics.java
@@ -24,14 +24,14 @@ public class Politics extends GenericSimulation
 	public Politics()
 	{
 
-		super("Basic Politics Testing Bed", 200, 0, 0.1);
+		super("Basic Politics Testing Bed", 2000, 0, 0.1);
 	}
 
 	@Override
 	protected void agents()
 	{
              Random randomGenerator = new Random();
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 100; i++)
 		{
                         addAgent(new TestPoliticalAgent(20, 2, AgentType.AC, randomGenerator.nextDouble(), randomGenerator.nextDouble()));
 			addAgent(new TestPoliticalAgent(20, 2, AgentType.TFT, randomGenerator.nextDouble(), randomGenerator.nextDouble()));
