034365c14d0c7c1cc690b6dda9d9615836fc9c7e
Xitong Gao <gxtfmx@gmail.com>
2011-06-14 23:02:18 +0100

    Added a fast simple main to test network (will go away later)

--- src/ise/gameoflife/neuralnetworks/TanhFeedForwardNetwork.java
+++ src/ise/gameoflife/neuralnetworks/TanhFeedForwardNetwork.java
@@ -1,5 +1,7 @@
 package ise.gameoflife.neuralnetworks;
 
+import java.util.Random;
+
 /**
  * @author Xitong
  * TODO TFFN doc
@@ -32,6 +34,57 @@ public class TanhFeedForwardNetwork extends Network
 
 		this.setLayers(layers);
 	}
+
+	public static void main(String args[])
+	{
+		TanhFeedForwardNetwork net = new TanhFeedForwardNetwork(4,5,3);
+		Random rand = new Random();
+		for (int i = 0; i < net.layers().length; i++)
+		{
+			Layer layer = net.layers()[i];
+			for (int j = 0; j < layer.neurons().length; j++)
+			{
+				Neuron neuron = layer.neurons()[j];
+				double weights[] = new double[neuron.inputs()];
+				for (int k = 0; k < weights.length; k++)
+				{
+					weights[k] = rand.nextDouble();
+				}
+				neuron.setWeights(weights);
+			}
+		}
+
+		// test value setup
+		double in[] = new double[net.inputs()];
+		for (int i = 0; i < net.inputs(); i++)
+		{
+			in[i] = rand.nextDouble();
+		}
+		// show test values
+		for (double val : in)
+		{
+			System.out.println("In: " + val);
+		}
+		// show output values
+		double out[] = net.out(in);
+		for (double val : out)
+		{
+			System.out.println("Out: " + val);
+		}
+		// show weights
+		double weights[][][] = net.weights();
+		for (double layerVals[][] : weights)
+		{
+			System.out.println("Layer");
+			for (double neuronVals[] : layerVals)
+			{
+				System.out.println("\tNeuron");
+				for (double val : neuronVals)
+				{
+					System.out.println("\t\tVal: " + val);
+				}
+			}
+		}
 	}
 
 }
