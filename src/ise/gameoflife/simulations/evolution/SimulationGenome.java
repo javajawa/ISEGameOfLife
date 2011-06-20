cf61d3b302a9d4dc6890bfaf57cb328e63f61c75
Xitong Gao <gxtfmx@gmail.com>
2011-06-20 14:43:39 +0100

    Update SimulationGenome with some necessary vals.

--- src/ise/gameoflife/simulations/evolution/SimulationGenome.java
+++ src/ise/gameoflife/simulations/evolution/SimulationGenome.java
@@ -14,6 +14,11 @@ public class SimulationGenome extends Genome<SimulationGenome>
 
 	private static final long serialVersionUID = 1L;
 
+	private final String comment = "Learning Agent";
+	private final int iterations = 200;
+	private final double foodConsumedPerAdvice = 0.1;
+	private long randomSeed;
+
 	/* (non-Javadoc)
 	 * @see ise.gameoflife.genetics.Genome#randomize()
 	 */
@@ -42,6 +47,31 @@ public class SimulationGenome extends Genome<SimulationGenome>
 	{
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public String comment()
+	{
+		return comment;
+	}
+
+	public int iterations()
+	{
+		return iterations;
+	}
+
+	public double foodConsumedPerAdvice()
+	{
+		return foodConsumedPerAdvice;
+	}
+
+	public void setRandomSeed(long seed)
+	{
+		this.randomSeed = seed;
+	}
+
+	public long randomSeed()
+	{
+		return randomSeed;
 	}
 
 }
