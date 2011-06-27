e792d6f0daaba7769c19219851469081a5bc13bb
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-27 19:07:11 +0100

    Document: simulation.validation.SingleAgent

--- src/ise/mace/simulations/validation/SingleAgent.java
+++ src/ise/mace/simulations/validation/SingleAgent.java
@@ -2,19 +2,43 @@ package ise.mace.simulations.validation;
 
 import ise.mace.agents.TestAgent;
 import ise.mace.participants.AbstractFreeAgentGroup;
-import ise.mace.plugins.database.DatabasePlugin;
 import ise.mace.plugins.DebugSwitchPlugin;
-import ise.mace.plugins.HunterListPlugin;
 import ise.mace.plugins.HuntersAlivePlugin;
-import ise.mace.plugins.PoliticalCompassPlugin;
 import ise.mace.simulations.GenericSimulation;
 
 /**
+ * <p>Single Agent Simulation</p>
  *
+ * <ul>
+ * <li>Cycles: 500</li>
+ * <li>Agents: 1 {@link TestAgent}
+ *   <ul>
+ *     <li>Initial Food: 20</li>
+ *     <li>Default Consumption: 5</li>
+ *   </ul>
+ * <li>Advice Consumption: 0</li>
+ * <li>Free Group: None</li>
+ * <li>Groups:
+ *   <ul>
+ *     <li>None</li>
+ *   </ul>
+ * </li>
+ * <li>Foods:
+ *   <ul>
+ *     <li>Rabbit:  1 from 1</li>
+ *     <li>Chicken: 2 from 1</li>
+ *   </ul>
+ * </li>
+ * <li>Database: None</li>
+ * </ul>
  */
 public class SingleAgent extends GenericSimulation
 {
 
+	/**
+	 * Creates a new {@link SingleAgent} simulation
+	 * @see SingleAgent
+	 */
 	public SingleAgent()
 	{
 		super("Single agent starving to death", 100, 0, 0);
@@ -49,9 +73,6 @@ public class SingleAgent extends GenericSimulation
 	{
 		addPlugin(new DebugSwitchPlugin());
 		addPlugin(new HuntersAlivePlugin(getPath() + "/population.png", 1500, 1200));
-		addPlugin(new DatabasePlugin("Simulation comment",false));
-		addPlugin(new HunterListPlugin());
-		addPlugin(new PoliticalCompassPlugin());
 	}
 
 	@Override
