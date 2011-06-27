00e594481659ebf2eb338fd0496ab464b8d5a67c
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-27 19:32:56 +0100

    Document: simulation.validation.BigRabbitDoubleAC

--- src/ise/mace/simulations/validation/BigRabbitDoubleAC.java
+++ src/ise/mace/simulations/validation/BigRabbitDoubleAC.java
@@ -11,11 +11,40 @@ import ise.mace.simulations.GenericSimulation;
 import ise.mace.tokens.AgentType;
 
 /**
+ * <p>The Big Rabbit Simulation</p>
  *
+ * <ul>
+ * <li>Cycles: 500</li>
+ * <li>Agents: 1 {@link TestPoliticalAgent}
+ *   <ul>
+ *     <li>Initial Food: 20</li>
+ *     <li>Default Consumption: 2</li>
+ *     <li>Beliefs: (0.5,0.5)</li>
+ *   </ul>
+ * <li>Advice Consumption: 0</li>
+ * <li>Free Group: {@link BasicFreeAgentGroup}</li>
+ * <li>Groups:
+ *   <ul>
+ *     <li>None</li>
+ *   </ul>
+ * </li>
+ * <li>Foods:
+ *   <ul>
+ *     <li>Rabbit: 5 from 1</li>
+ *     <li>Stag:   2 from 2</li>
+ *   </ul>
+ * </li>
+ * <li>Database: None</li>
+ * <li>Default seed: 0</li>
+ * </ul>
  */
 public class BigRabbitDoubleAC extends GenericSimulation
 {
 
+	/**
+	 * Crates the Big Rabbit Validation Simulation
+	 * @see BigRabbitDoubleAC Big Rabbit Simulation
+	 */
 	public BigRabbitDoubleAC()
 	{
 		super("Single agent starving to death", 500, 0, 0);
