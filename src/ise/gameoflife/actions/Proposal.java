57942b46124f38df6142167325b777654051e14d
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-27 11:19:37 +0100

    Clean up: action.Proposal

--- src/ise/gameoflife/actions/Proposal.java
+++ src/ise/gameoflife/actions/Proposal.java
@@ -12,6 +12,13 @@ public class Proposal extends GenericAction
 	 * Serialisation ID
 	 */
 	private static final long serialVersionUID = 1L;
+	/**
+	 * Enum that lists the different propositions that can be made in the voting
+	 * stage.
+	 *
+	 * The properties of each value determine how the proposition works; currently
+	 * this is limited to a shift in economic location only
+	 */
 	public enum ProposalType
 	{
 		/**
@@ -27,8 +34,15 @@ public class Proposal extends GenericAction
 		 */
 		moveRight(1);
 
+		/**
+		 * The change in economic position
+		 */
 		private double movement;
 
+		/**
+		 * Creates a instance of the enum
+		 * @param movement The change in the economic position
+		 */
 		private ProposalType(double movement)
 		{
 			this.movement = movement;
@@ -44,20 +58,39 @@ public class Proposal extends GenericAction
 		}
 	}
 
+	/**
+	 * The type of proposal that is being created
+	 */
 	private ProposalType type;
+	/**
+	 * The group in which this proposition is being made
+	 */
 	private String forGroup;
 
+	/**
+	 * Creates a new proposal action
+	 * @param type The type of proposal
+	 * @param forGroup The group where it's being made
+	 */
 	public Proposal(ProposalType type, String forGroup)
 	{
 		this.type = type;
 		this.forGroup = forGroup;
 	}
 
+	/**
+	 * Returns the type of proposal that is being created
+	 * @return The type of proposal that is being created
+	 */
 	public ProposalType getType()
 	{
 		return type;
 	}
 
+	/**
+	 * Returns the group in which this proposition is being made
+	 * @return The group in which this proposition is being made
+	 */
 	public String getForGroup()
 	{
 		return forGroup;
