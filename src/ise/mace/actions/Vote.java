d487afda60d79582153985dfb047bd1ed59109fd
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-27 15:09:49 +0100

    Formatting, Document: actions.Vote

--- src/ise/mace/actions/Vote.java
+++ src/ise/mace/actions/Vote.java
@@ -3,15 +3,15 @@ package ise.mace.actions;
 import ise.mace.inputs.Proposition;
 
 /**
- *
+ * Allows an Agent to vote on a {@link Proposition}
  */
 public class Vote extends GenericAction
 {
-
 	/**
 	 * Serialisation ID
 	 */
 	private static final long serialVersionUID = 1L;
+
 	/**
 	 * Enum to describe the different actions that can be taken in a vote
 	 */
@@ -29,7 +29,6 @@ public class Vote extends GenericAction
 		 * Agent is not using it's vote on this proposition
 		 */
 		Abstain(0);
-
 		/**
 		 * The 'movement' of this vote - increase / decrease to the overall vote
 		 * value
@@ -45,29 +44,51 @@ public class Vote extends GenericAction
 			this.value = value;
 		}
 
+		/**
+		 * Returns the movement value of this vote
+		 * @return The movement value of this vote
+		 */
 		public int getValue()
 		{
 			return value;
 		}
 	}
 
+	/**
+	 * The proposition being voted on
+	 */
 	private Proposition proposition;
+	/**
+	 * The vote being cast
+	 */
 	private VoteType vote;
 
+	/**
+	 * Creates a new vote action
+	 * @param proposition The proposition being voted on
+	 * @param vote The vote being cast
+	 */
 	public Vote(Proposition proposition, VoteType vote)
 	{
 		this.proposition = proposition;
 		this.vote = vote;
 	}
 
+	/**
+	 * The proposition being voted on
+	 * @return The proposition
+	 */
 	public Proposition getProposition()
 	{
 		return proposition;
 	}
 
+	/**
+	 * The vote being cast
+	 * @return The vote
+	 */
 	public VoteType getVote()
 	{
 		return vote;
 	}
-
 }
