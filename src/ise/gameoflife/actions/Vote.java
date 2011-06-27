3bf1835e3e7ec5885ac38d31f43e8db6521219d0
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-27 11:19:44 +0100

    Clean up: actions.Vote

--- src/ise/gameoflife/actions/Vote.java
+++ src/ise/gameoflife/actions/Vote.java
@@ -13,14 +13,34 @@ public class Vote extends GenericAction
 	 * Serialisation ID
 	 */
 	private static final long serialVersionUID = 1L;
+	/**
+	 * Enum to describe the different actions that can be taken in a vote
+	 */
 	public enum VoteType
 	{
+		/**
+		 * Agent is voting to enact this proposition
+		 */
 		For(1),
+		/**
+		 * Agent is voting to <em>not</em> enact this proposition
+		 */
 		Against(-1),
+		/**
+		 * Agent is not using it's vote on this proposition
+		 */
 		Abstain(0);
 
+		/**
+		 * The 'movement' of this vote - increase / decrease to the overall vote
+		 * value
+		 */
 		private int value;
 
+		/**
+		 * Constructs an instance of the enum
+		 * @param value The movement of the vote
+		 */
 		private VoteType(int value)
 		{
 			this.value = value;
