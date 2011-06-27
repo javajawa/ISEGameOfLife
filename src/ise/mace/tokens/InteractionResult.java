24ac8d4df33a23bd5eccdfe7bd88bf5c531a770b
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-27 20:21:14 +0100

    Format: token.InteractionResult

--- src/ise/mace/tokens/InteractionResult.java
+++ src/ise/mace/tokens/InteractionResult.java
@@ -2,14 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ise.mace.tokens;
 
 /**
  *
  */
-public enum InteractionResult {
-    LoanGiven, //Loan has been given to another group
-    LoanTaken, //Group has received financial aid from another group
-    NothingHappened, //Interaction didn't cause any changes
+public enum InteractionResult
+{
+	LoanGiven, //Loan has been given to another group
+	LoanTaken, //Group has received financial aid from another group
+	NothingHappened, //Interaction didn't cause any changes
 }
