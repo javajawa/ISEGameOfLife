04e009febe0d9a4bb8772bf4c5a57345673074a1
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-27 12:04:35 +0100

    Clean up: BuildSimulations

--- src/ise/gameoflife/BuildSimulations.java
+++ src/ise/gameoflife/BuildSimulations.java
@@ -12,15 +12,20 @@ import java.util.logging.Logger;
 
 /**
  * Magic simulation building class
- * Add classes to the simulationClasses const to have them auto-compiled
- * @author Benedict
+ * Add classes to {@link #simulationClasses} to have them auto-compiled
  */
 final class BuildSimulations
 {
 
 	/**
-	 * The list of different simulation classes that can be built without
-	 * initialisation parameters.
+	 * <p>The list of different simulation classes that can be built without
+	 * initialisation parameters.</p>
+	 *
+	 * <p>If you design a simulation that requires a random seed, it can be built on
+	 * the command line with:<br />
+	 * <code>&lt;Classname&gt; &lt;seed&gt;</code><br />
+	 * e.g. <code>java -jar Mace.jar CLIPolitics 23</code>
+	 * </p>
 	 */
 	private static final Class[] simulationClasses =
 	{
