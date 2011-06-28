c78362b55ce2cb5234d554a458daabf610fe65b9
Olly <OGHill@googlemail.com>
2011-06-28 23:10:26 +0100

    Documented Hungry Double AD and Hungry double AC
    Signed-off-by: Olly <OGHill@googlemail.com>

--- src/ise/mace/simulations/validation/HungryDoubleAD.java
+++ src/ise/mace/simulations/validation/HungryDoubleAD.java
@@ -11,10 +11,41 @@ import ise.mace.simulations.GenericSimulation;
 import ise.mace.tokens.AgentType;
 
 /**
+ * <p>The Hungry Defecting Double/p>
+ * <p><strong>Proposition:</strong> Two always defecting agents will die despite the fact
+ *	that teamwork could get them stags and let them live.</p>
  *
+ * <ul>
+ * <li>Cycles: 500</li>
+ * <li>Agents: 2 {@link TestAgent}
+ *   <ul>
+ *     <li>Initial Food: 20</li>
+ *     <li>Default Consumption: 2</li>
+ *   </ul>
+ * <li>Advice Consumption: 0</li>
+ * <li>Free Group: None</li>
+ * <li>Groups:
+ *   <ul>
+ *     <li>None</li>
+ *   </ul>
+ * </li>
+ * <li>Foods:
+ *   <ul>
+ *     <li>Rabbit:  1 from 1</li>
+ *     <li>Stag: 5 from 2</li>
+ *   </ul>
+ * </li>
+ * <li>Database: None</li>
+ * <li>Default seed: 0</li>
+ * </ul>
  */
+
 public class HungryDoubleAD extends GenericSimulation
 {
+	/**
+	 * Creates a new {@link HungryDoubleAD} simulation
+	 * @see HungryDoubleAD
+	 */
 	public HungryDoubleAD()
 	{
 		super("Single agent starving to death", 500, 0, 0);
