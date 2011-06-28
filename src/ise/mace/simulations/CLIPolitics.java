b263ef03ae8006571c981f50da60157de984894c
Olly <OGHill@googlemail.com>
2011-06-28 22:16:11 +0100

    Documented CLIPolitics Simulation
    Signed-off-by: Olly <OGHill@googlemail.com>

--- src/ise/mace/simulations/CLIPolitics.java
+++ src/ise/mace/simulations/CLIPolitics.java
@@ -10,10 +10,45 @@ import ise.mace.tokens.AgentType;
 import java.util.Random;
 
 /**
+ * <p>CLIPolitics Simulation/p>
+ *
+ * <ul>
+ * <li>Cycles: 5000</li>
+ * <li>Agents: 200 {@link TestPoliticalAgent}s
+ *   <ul>
+ *     <li>50 of each Strategy in {@link AgentType}</li>
+ *     <li>Initial Food: 20</li>
+ *     <li>Default Consumption: 2</li>
+ *     <li>Beliefs: Randomised</li>
+ *   </ul>
+ * <li>Advice Consumption: 0.1</li>
+ * <li>Free Group: {@link BasicFreeAgentGroup}</li>
+ * <li>Groups:
+ *   <ul>
+ *     <li>{@link TestPoliticalGroup}</li>
+ *   </ul>
+ * </li>
+ * <li>Foods:
+ *   <ul>
+ *     <li>Rabbit: 2 from 1</li>
+ *     <li>Stag: 10 from 2</li>
+ *   </ul>
+ * </li>
+ * <li>Database: Primary Remote, Corvette</li>
+ * <li>Default seed: random (passed as parameter)</li>
+ * </ul>
+ */
+
+/**
  *
  */
 public class CLIPolitics extends GenericSimulation
 {
+	/**
+	 * Creates a CLIPolitics simulation
+	 * @see CLIPolitics
+	 * @param rand Random Seed
+	 */
 	public CLIPolitics(long rand)
 	{
 		super("Command Line Basic Politics Testing Bed " + rand, 5000, rand, 0.1,
