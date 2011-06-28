40f3142bbdb08f69977ceb83ae08a877757d3993
Olly <OGHill@googlemail.com>
2011-06-28 23:14:53 +0100

    Refactored SatisfiedDoubleAC into StupidSharing, and documented SS.
    Signed-off-by: Olly <OGHill@googlemail.com>
    
    SS was correctly implemented, but named as SDAC. After some puzzling, it was renamed.

--- src/ise/mace/simulations/validation/StupidSharing.java
+++ src/ise/mace/simulations/validation/StupidSharing.java
@@ -11,13 +11,46 @@ import ise.mace.simulations.GenericSimulation;
 import ise.mace.tokens.AgentType;
 
 /**
+ * <p>Stupid Sharing/p>
+ * <p><strong>Proposition:</strong> If one agent needs more food than the other, agents still distribute
+ * the food equally. This can lead to the scenario where two agents hunt together, with one living
+ * happily, but the other gradually starving. When the other dies, the remaining agent can only
+ * hunt rabbits, and so it dies too.</p>
  *
+ * <ul>
+ * <li>Cycles: 500</li>
+ * <li>Agents: 2 {@link TestAgent}, one with default Food consumption, one with a food consumption of 4
+ *   <ul>
+ *     <li>Initial Food: 20</li>
+ *     <li>Default Consumption: 2</li>
+ *   </ul>
+ * <li>Advice Consumption: 0</li>
+ * <li>Free Group: None</li>
+ * <li>Groups:
+ *   <ul>
+ *     <li>None</li>
+ *   </ul>
+ * </li>
+ * <li>Foods:
+ *   <ul>
+ *     <li>Rabbit:  1 from 1</li>
+ *     <li>Stag: 6 from 2</li>
+ *   </ul>
+ * </li>
+ * <li>Database: None</li>
+ * <li>Default seed: 0</li>
+ * </ul>
  */
-public class SatisfiedDoubleAC extends GenericSimulation
+
+public class StupidSharing extends GenericSimulation
 {
-	public SatisfiedDoubleAC()
+	/**
+	 * Creates a {@link StupidSharing} simulation
+	 * @see StupidSharing
+	 */
+	public StupidSharing()
 	{
-		super("Single agent starving to death", 500, 0, 0);
+		super("Two agents starving to death", 500, 0, 0);
 	}
 
 	@Override
