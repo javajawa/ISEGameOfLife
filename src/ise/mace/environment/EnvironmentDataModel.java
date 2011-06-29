07b8b9496de5b4f7d7bcc1f90eb4722a831a132b
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-29 10:20:21 +0100

    Document: environment.EnvironmentDataModel

--- src/ise/mace/environment/EnvironmentDataModel.java
+++ src/ise/mace/environment/EnvironmentDataModel.java
@@ -1,13 +1,17 @@
 package ise.mace.environment;
 
+import ise.mace.actions.Death;
 import ise.mace.participants.PublicAgentDataModel;
 import ise.mace.models.Food;
 import ise.mace.models.GroupDataInitialiser;
+import ise.mace.participants.AbstractAgent;
 import ise.mace.participants.AbstractGroupAgent;
 import ise.mace.participants.PublicGroupDataModel;
 import ise.mace.tokens.GroupRegistration;
 import ise.mace.tokens.RegistrationRequest;
+import ise.mace.tokens.RegistrationResponse;
 import ise.mace.tokens.TurnType;
+import ise.mace.tokens.UnregisterRequest;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -21,6 +25,8 @@ import java.util.UUID;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.ElementMap;
+import presage.Participant;
+import presage.Simulation;
 import presage.environment.AEnvDataModel;
 
 /**
@@ -28,42 +34,71 @@ import presage.environment.AEnvDataModel;
  */
 public class EnvironmentDataModel extends AEnvDataModel
 {
+	/**
+	 * Serialisation ID
+	 */
 	private final static long serialVersionUID = 1L;
 	/**
 	 * A sorted list/map of all the state of all players in the game
+	 * @see #getAgents()
 	 */
 	@ElementMap(keyType = String.class, valueType = PublicAgentDataModel.class)
 	private TreeMap<String, PublicAgentDataModel> agents = new TreeMap<String, PublicAgentDataModel>();
 	/**
 	 * List of all the available food types in the environment
+	 * @see #availableFoods()
 	 */
 	@ElementMap
 	private HashMap<String, Food> availableFoodTypes;
 	/**
 	 * List of all the groups in the environment
+	 * @see #getGroups()
 	 */
 	@ElementMap
 	private HashMap<String, PublicGroupDataModel> agentGroups;
 	/**
-	 *
+	 * List of permitted group types
+	 * @see #getAllowedGroupTypes()
 	 */
 	@ElementList(type = Class.class, required = false)
 	private ArrayList<Class<? extends AbstractGroupAgent>> allowedGroupTypes;
 	/**
-	 *
+	 * The currnet turn type
+	 * @see #getTurnType()
+	 * @see TurnType
 	 */
 	@Element
 	private TurnType turn;
+	/**
+	 * <p>The number of completed rounds</p>
+	 * <p>A round consists of exactly one of each of the {@link TurnType
+	 * Turn Types}. The value is incremented in {@link #setTime(long) setTime}
+	 * every time the {@link TurnType#firstTurn first turn} is selected as the
+	 * current {@link #turn turn}</p>
+	 */
 	@Element
 	private int rounds;
+	/**
+	 * UUID for this object
+	 * @see #getId()
+	 */
 	@Element
 	private String id;
+	/**
+	 * <p>The amount of food that is consumed by an {@link AbstractAgent agent}
+	 * when they {@link AbstractAgent#seekAvice(java.lang.String) seek advice}
+	 * from another agent.</p>
+	 * <p>This is designed to model the amount of time and resources spent on
+	 * communication and getting the advice, but is only currently taken from the
+	 * agent seeking the advice.</p>
+	 * @see #getFoodConsumedPerAdvice()
+	 */
 	@Element
 	private double foodConsumedPerAdvice;
 
 	/**
 	 * Serialisable no-arg constructor, do not use
-	 * @deprecated
+	 * @deprecated For serialisation only
 	 */
 	@Deprecated
 	public EnvironmentDataModel()
@@ -71,43 +106,89 @@ public class EnvironmentDataModel extends AEnvDataModel
 		super();
 	}
 
+	/**
+	 * Creates a new environment data model for constructing an {@link
+	 * Environment} in a {@link Simulation}
+	 * @param environmentName The name to give the environment
+	 * @param availableFoodTypes The food that agents can hunt {@link
+	 * #availableFoods()}
+	 * @param allowedGroupTypes The groups for use. {@link
+	 * #getAllowedGroupTypes()}
+	 * @param foodConsumedPerAdvice Amount of food consumed when an {@link
+	 * AbstractAgent agent} {@link AbstractAgent#seekAvice(java.lang.String) seeks
+	 * adive}. See {@link #foodConsumedPerAdvice}
+	 */
 	public EnvironmentDataModel(String environmentName,
-					HashMap<String, Food> availableFoodTypes,
-					ArrayList<Class<? extends AbstractGroupAgent>> allowedGroupTypes,
-					double foodConsumedPerAdvice)
+	    HashMap<String, Food> availableFoodTypes,
+	    List<Class<? extends AbstractGroupAgent>> allowedGroupTypes,
+	    double foodConsumedPerAdvice)
 	{
 		super(environmentName, "ISE Game of Life Enviroment Data Model", 0);
 		this.availableFoodTypes = availableFoodTypes;
 
 		this.agentGroups = new HashMap<String, PublicGroupDataModel>();
-		this.allowedGroupTypes = allowedGroupTypes;
+		this.allowedGroupTypes =
+		    new ArrayList<Class<? extends AbstractGroupAgent>>(allowedGroupTypes);
 
 		this.turn = TurnType.firstTurn;
 		this.rounds = 0;
 		this.id = UUID.randomUUID().toString();
 	}
 
+	/**
+	 * Finds what food types are available to hunt
+	 * @return set of available food.
+	 * @see #getFoodById(java.util.UUID)
+	 */
 	public Set<Food> availableFoods()
 	{
 		return Collections.unmodifiableSet(new HashSet<Food>(
 						availableFoodTypes.values()));
 	}
 
+	/**
+	 * Gets the food object associated with a particular id
+	 * @param id The id to search for
+	 * @return The food object, or null if not found
+	 * @see #availableFoods()
+	 */
 	public Food getFoodById(UUID id)
 	{
 		return availableFoodTypes.get(id.toString());
 	}
 
+	/**
+	 * Gets the agent data object associated with a particular id, which is safe
+	 * for being passed to other agents without giving them too much information
+	 * @param id The id to search for
+	 * @return The agent object, or null if not found
+	 * @see #getAgents()
+	 */
 	public PublicGroupDataModel getGroupById(String id)
 	{
 		return agentGroups.get(id);
 	}
 
+	/**
+	 * Gets the group object associated with a particular id
+	 * @param id The id to search for
+	 * @return The group object, or null if not found
+	 * @see #getGroups()
+	 */
 	public PublicAgentDataModel getAgentById(String id)
 	{
 		return agents.get(id);
 	}
 
+	/**
+	 * Removes an {@link AbstractAgent agents} or {@link AbstractGroupAgent group}
+	 * from the simulation
+	 * @param id The id of the participant to remove
+	 * @return Whether the {@link Participant} could be removed
+	 * @see #registerParticipant(ise.mace.tokens.RegistrationRequest)
+	 * @see UnregisterRequest
+	 * @see Death
+	 */
 	public boolean removeParticipant(String id)
 	{
 		if (agents.remove(id) == null)
@@ -116,29 +197,74 @@ public class EnvironmentDataModel extends AEnvDataModel
 		}
 		return true;
 	}
-
+	/**
+	 * Registers an {@link AbstractAgent agent} as part of the Environment
+	 * @param id the new group
+	 * @return Whether the registration was successful
+	 * @see #registerGroup(ise.mace.tokens.GroupRegistration)
+	 * @see RegistrationRequest
+	 * @see RegistrationResponse
+	 */
 	boolean registerParticipant(RegistrationRequest id)
 	{
 		agents.put(id.getParticipantID(), id.getModel());
 		return true;
 	}
 
+	/**
+	 * Registers a {@link AbstractGroupAgent group} as part of the Environment
+	 * @param ng the new group
+	 * @return Whether the registration was successful
+	 * @see #registerParticipant(ise.mace.tokens.RegistrationRequest)
+	 * @see GroupRegistration
+	 * @see RegistrationResponse
+	 */
 	boolean registerGroup(GroupRegistration ng)
 	{
 		agentGroups.put(ng.getParticipantID(), ng.getDm());
 		return true;
 	}
 
+	/**
+	 * Returns what {@link TurnType turn} it is in the round
+	 * @return The current turn type
+	 * @see #getRoundsPassed()
+	 */
 	public TurnType getTurnType()
 	{
 		return turn;
 	}
 
+	/**
+	 * Returns the list of all classes of group that may be used in this
+	 * simulation.
+	 *
+	 * Each of these classes will extend {@link AbstractGroupAgent}.
+	 * The validity of this classes is not checked at simulation time - the
+	 * framework may not be able to initialise them with a {@link
+	 * GroupDataInitialiser}.
+	 *
+	 * The returned classes are also not guaranteed to be concrete
+	 * implementations.
+	 *
+	 * Groups of these classes can then be created with {@link
+	 * #createGroup(java.lang.Class, ise.mace.models.GroupDataInitialiser)
+	 * createGroup()} with an appropriate initialiser.
+	 * @return List of all permitted Group classes
+	 * @see AbstractGroupAgent
+	 * @see GroupDataInitialiser
+	 * @see #createGroup(java.lang.Class, ise.mace.models.GroupDataInitialiser)
+	 * @see #getGroups()
+	 */
 	public List<Class<? extends AbstractGroupAgent>> getAllowedGroupTypes()
 	{
 		return Collections.unmodifiableList(allowedGroupTypes);
 	}
 
+	/**
+	 * Updates the time in the model
+	 * @param time New cycle time
+	 */
 	@Override
 	public void setTime(long time)
 	{
@@ -161,6 +287,11 @@ public class EnvironmentDataModel extends AEnvDataModel
 		turn = t[next];
 	}
 
+	/**
+	 * Gets the total number of rounds past
+	 * @return Total completed rounds
+	 * @see TurnType
+	 */
 	public int getRoundsPassed()
 	{
 		return rounds;
