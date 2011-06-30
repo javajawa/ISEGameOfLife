0376679e4d152ad6db7a13210d8f9ad89a19595a
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-30 10:33:54 +0100

    Docuemnt: tokens.TurnType

--- src/ise/mace/tokens/TurnType.java
+++ src/ise/mace/tokens/TurnType.java
@@ -1,7 +1,18 @@
 package ise.mace.tokens;
-
+import ise.mace.environment.EnvironmentDataModel;
+import presage.Presage;
 /**
- * Used to control which turn type it is
+ * <p>Enumoratored Class that determines the stage of the games</p>
+ * <p>Each cycle that {@link Presage} simulates, which is equivalent to each
+ * {@link EnvironmentDataModel#time time stamp} has an associated turn, which
+ * can be found in {@link EnvironmentDataModel#turn}.
+ * These turns are cycles through based on the constant {@link #values() values}
+ * array, which follows the order of declaration within the class.
+ * A series of cycles consisting of all of the turn in <code>values</code> is
+ * known as a round, and the current round number is stored at {@link
+ * EnvironmentDataModel#rounds}.
+ * Rounds begin with the turn specified in the {@link #firstTurn first turn}
+ * constant.</p>
  */
 public enum TurnType
 {
@@ -13,18 +24,24 @@ public enum TurnType
 	 * Group splits agents into hunting teams
 	 */
 	TeamSelect,
-	/*
+	/**
 	 * Agent specifies which food it wishes to obtain
 	 */
 	GoHunt,
-	/*
+	/**
 	 * Environment states what the result of each hunt has been
 	 */
 	HuntResults,
+	/**
+	 * Agents make proposals to their group
+	 */
 	MakeProposals,
+	/**
+	 * Votes data is returned from the previous round, and data is updated
+	 */
 	Voting;
 	/**
-	 * The first type in a round
+	 * The first turn type in a round
 	 */
 	public final static TurnType firstTurn = values()[0];
 }
